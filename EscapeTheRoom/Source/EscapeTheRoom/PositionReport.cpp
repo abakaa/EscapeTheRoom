f2bfdbfa469c84e82a01e8574d77f0533742ecb4
Arda Altay Abaka <arda@Ardas-MacBook-Pro.local>
2017-08-23 22:18:48 +0300

    Minor fix

--- EscapeTheRoom/Source/EscapeTheRoom/PositionReport.cpp
+++ EscapeTheRoom/Source/EscapeTheRoom/PositionReport.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
 
 
 // Sets default values for this component's properties
@@ -9,7 +11,8 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
+    
+    
 	// ...
 }
 
@@ -18,7 +21,12 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
+    
+    FString objName = GetOwner()->GetName();
+    FString objPos = GetOwner()->GetActorLocation().ToString();
+    
+    
+    UE_LOG(LogTemp, Warning, TEXT("%s named object is at %s"), *objName, *objPos);
 	// ...
 	
 }
